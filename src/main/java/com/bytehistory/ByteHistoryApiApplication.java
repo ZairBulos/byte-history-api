926a5421635e079c6c42d78e3912540ab3c2653f
Zair Bulos <buloszair@gmail.com>
2025-07-04 13:26:28 -0300

    chore(api): initial project setup

---
+++ src/main/java/com/bytehistory/ByteHistoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.bytehistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteHistoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteHistoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytehistory/ByteHistoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytehistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteHistoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
