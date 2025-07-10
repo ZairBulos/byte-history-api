9aa2fb46a07566dbd70455c7a4d8d4b7cb5670f8
Zair Bulos <buloszair@gmail.com>
2025-07-10 12:48:39 -0300

    feat(api): add caching with ConcurrentMapCacheManager and enable @Cacheable in TechMilestoneService

--- src/main/java/com/bytehistory/services/TechMilestoneService.java
+++ src/main/java/com/bytehistory/services/TechMilestoneService.java
@@ -5,6 +5,7 @@ import com.bytehistory.data.repositories.TechMilestoneRepository;
 import com.bytehistory.services.exceptions.NotFoundException;
 import com.bytehistory.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class TechMilestoneService {
     private final TechMilestoneRepository repository;
 
+    @Cacheable("techMilestone")
     @Transactional(readOnly = true)
     public TechMilestoneResponse get() {
-        var milestone = repository.findTodayMilestone().orElseThrow(NotFoundException::new);
-        return new TechMilestoneResponse(milestone.getMilestone(), milestone.getAiModel(), DateUtils.getCurrentDate());
+        System.out.println("Service called");
+        return repository.findTodayMilestone()
+                .map(m -> new TechMilestoneResponse(m.getMilestone(), m.getAiModel(), DateUtils.getCurrentDate()))
+                .orElseThrow(NotFoundException::new);
     }
 }
