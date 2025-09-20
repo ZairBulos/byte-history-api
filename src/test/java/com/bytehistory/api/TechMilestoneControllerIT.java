26877ba39d0b1d8587e95c330d6649ae727d5323
Zair Bulos <buloszair@gmail.com>
2025-09-20 19:39:40 -0300

    feat(api): remove rate limit filter

--- src/test/java/com/bytehistory/api/TechMilestoneControllerIT.java
+++ src/test/java/com/bytehistory/api/TechMilestoneControllerIT.java
@@ -1,8 +1,6 @@
 package com.bytehistory.api;
 
 import com.bytehistory.api.dtos.TechMilestoneResponse;
-import com.bytehistory.api.filters.RateLimitFilter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -15,14 +13,6 @@ class TechMilestoneControllerIT {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Autowired
-    private RateLimitFilter rateLimitFilter;
-
-    @BeforeEach
-    void beforeEach() {
-        rateLimitFilter.clearCache();
-    }
-
     @Test
     void shouldReturnTodayMilestone() {
         var response = restTemplate.getForEntity("/api/tech-milestones", TechMilestoneResponse.class);
@@ -33,15 +23,5 @@ class TechMilestoneControllerIT {
         assertThat(body.milestone()).isEqualTo("Test Milestone");
         assertThat(body.model()).isEqualTo("test-model");
         assertThat(body.date()).isNotBlank();
-    }
-
-    @Test
-    void shouldReturn429AfterExceedingRateLimit() {
-        for (int i = 0; i < 10; i++) {
-            restTemplate.getForEntity("/api/tech-milestones", TechMilestoneResponse.class);
-        }
-
-        var response = restTemplate.getForEntity("/api/tech-milestones", TechMilestoneResponse.class);
-        assertThat(response.getStatusCode().value()).isEqualTo(429);
     }
 }
